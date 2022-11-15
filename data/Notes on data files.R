446c12e2871f8fe736cab50ce9d86b80bcac8aac
evancf <ef13@rice.edu>
2022-11-15 14:47:00 -0800

    Planning folder structure for data

---
+++ data/Notes on data files.R
@@ -0,0 +1,12 @@
+# Because we won't share large files over github, here are some notes on what this folder should contain
+
+# Input data
+
+# TRY data, which is a tab separated txt file currently called 13238.txt
+# Will want multimedia.txt in here too 
+
+
+
+# Output data / data saved at certain steps
+
+# This is things like the 'species_links.txt' and 'Dat_full_ready_for_sampling.txt'
