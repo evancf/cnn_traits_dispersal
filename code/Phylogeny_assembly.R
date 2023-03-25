ac5c4c0ae4e68a717113f394e0ac2494ec53a7f0
Hardy <hardyxu@utexas.edu>
2023-03-25 18:54:48 -0400

    Fix Output Issue

--- code/Phylogeny_assembly.R
+++ code/Phylogeny_assembly.R
@@ -6,13 +6,14 @@ setwd("~/project/cnn_traits_dispersal/")
 
 # import sp_dat for species data
 load("./data/sp_dat.RData")
+sp.list <- sp_dat
 megatree <- read.tree("./data/plant_megatree.tre")
-families <- read.csv("./data/plant_genus_list.csv")
+gen.list <- read.csv("./data/plant_genus_list.csv")
 
 
-dim(sp_dat)
+dim(sp.list)
 time.phylo.start <- Sys.time()
-sp_tree <- phylo.maker(sp_dat, megatree, families, nodes.type = 1, scenario = 3) # Started around 5:00 PM Wednesday
+sp_tree <- phylo.maker(sp.list, megatree, list, nodes.type = 1, scenario = 3) # Started around 5:00 PM Wednesday
 time.phylo.end <- Sys.time()
 time.phylo.end - time.phylo.start # Took about 16 hours
 
@@ -23,5 +24,5 @@ save(sp_tree, file = "./data/sp_tree.RData")
 write.tree(sp_tree$phylo, file = "~/data/Phylogeny/seed_plant_phylo.tre")
 
 # Write the phylogeny out for RevBayes analysis
-write.csv(sp_tree$sp_dat,
+write.csv(sp_tree$sp.list,
            file = "~/data/Phylogeny/seed_plant_phylo.txt")
